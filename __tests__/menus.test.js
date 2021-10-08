0302f9eeb05cdcb557cfcacb937d6bb52d3f25a7
BenFrancka <benfrancka@gmail.com>
2021-10-08 15:24:03 -0700

    adds initial failing test for get by id for menus

--- __tests__/menus.test.js
+++ __tests__/menus.test.js
@@ -17,36 +17,76 @@ describe('Bienventory-be inventory routes', () => {
 
   it('adds a menu item using post', async () => {
     await User.insert({
-        google_id: '12345',
-        notifications: true,
-      });
-      await Inventory.insert({
-        user_id: '12345',
-        item_name: 'potatoes',
-        description: 'wots taters precious',
-        total_on_hand: 10,
-        par: 50,
-        unit_type: 'pounds',
-      });
-      await Inventory.insert({
-        user_id: '12345',
-        item_name: 'butter',
-        description: 'unsalted butter',
-        total_on_hand: 30,
-        par: 4,
-        unit_type: 'pounds',
-      });
-      const hashbrowns = {
-          inventory_id: '1',
-          meal_name: 'hashbrowns',
-          ingredients: [{ name: 'potatoes', quantity: 1 }, { name: 'butter', quantity: 1/4 }]
-      }
-      const res = await request(app)
-        .post('/api/v1/menus').send(hashbrowns);
+      google_id: '12345',
+      notifications: true,
+    });
+    await Inventory.insert({
+      user_id: '12345',
+      item_name: 'potatoes',
+      description: 'wots taters precious',
+      total_on_hand: 10,
+      par: 50,
+      unit_type: 'pounds',
+    });
+    await Inventory.insert({
+      user_id: '12345',
+      item_name: 'butter',
+      description: 'unsalted butter',
+      total_on_hand: 30,
+      par: 4,
+      unit_type: 'pounds',
+    });
+    const hashbrowns = {
+      inventory_id: '1',
+      meal_name: 'hashbrowns',
+      ingredients: [
+        { name: 'potatoes', quantity: 1 },
+        { name: 'butter', quantity: 1 / 4 },
+      ],
+    };
+    const res = await request(app).post('/api/v1/menus').send(hashbrowns);
 
-        expect(res.body).toEqual({ id: '1', 
-        inventory_id: '1',
-        meal_name: 'hashbrowns',
-        ingredients: [JSON.stringify({ name: 'potatoes', quantity: 1 }), JSON.stringify({ name: 'butter', quantity: 1/4 })] })
-  })
+    expect(res.body).toEqual({
+      id: '1',
+      inventory_id: '1',
+      meal_name: 'hashbrowns',
+      ingredients: [
+        JSON.stringify({ name: 'potatoes', quantity: 1 }),
+        JSON.stringify({ name: 'butter', quantity: 1 / 4 }),
+      ],
+    });
+  });
+
+  it('gets a menu item by id using GET', async () => {
+    await User.insert({
+      google_id: '12345',
+      notifications: true,
+    });
+    await Inventory.insert({
+      user_id: '12345',
+      item_name: 'potatoes',
+      description: 'wots taters precious',
+      total_on_hand: 10,
+      par: 50,
+      unit_type: 'pounds',
+    });
+    await Inventory.insert({
+      user_id: '12345',
+      item_name: 'butter',
+      description: 'unsalted butter',
+      total_on_hand: 30,
+      par: 4,
+      unit_type: 'pounds',
+    });
+    const hashbrowns = await Menu.insert({
+      inventory_id: '1',
+      meal_name: 'hashbrowns',
+      ingredients: [
+        { name: 'potatoes', quantity: 1 },
+        { name: 'butter', quantity: 1 / 4 },
+      ],
+    });
+    const res = await request(app).get(`/api/v1/menus/${hashbrowns.id}`);
+    expect(res.body).toEqual(hashbrowns);
+  });
 });
