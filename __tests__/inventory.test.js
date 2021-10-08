2917f9f70e738b9a28c37b8415096ae004e8db51
Isaac <isaac.ewing3@gmail.com>
2021-10-08 11:00:17 -0600

    inventory post route working, get by id test failing

--- __tests__/inventory.test.js
+++ __tests__/inventory.test.js
@@ -14,6 +14,10 @@ describe('Bienventory-be inventory routes', () => {
   });
 
 it('creating a new inventory item with POST', async () => {
+    const newUser = User.insert({
+        google_id: '12345',
+        notifications: true,
+      });
     const newItem = {
         user_id: '12345',
         item_name: 'milk',
@@ -23,8 +27,23 @@ it('creating a new inventory item with POST', async () => {
         unit_type: 'gallons'
     };
 
-    const res = await (await request(app).post('/api/v1/inventory')).send(newItem);
+    const res = await request(app).post('/api/v1/inventory').send(newItem);
 
-    expect(res.body).toEqual(newItem);
+    expect(res.body).toEqual({ id: '1', ...newItem });
+});
+
+it('gets an inventory item by id', async () => {
+    const newItem = {
+        user_id: '12345',
+        item_name: 'milk',
+        description: 'its milk',
+        total_on_hand: 4,
+        par: 2,
+        unit_type: 'gallons'
+    };
+
+    const newInventoryItem = await Inventory.insert(newItem);
+    const res = await request(app).get(`/api/v1/inventory/${newInventoryItem.id}`);
+    expect(res.body).toEqual(newInventoryItem);
 })
 })
