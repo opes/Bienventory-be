c169bdb7be786a11970cb548cd202f922ef20e8a
Dan Minkevitch <dan@alchemycodelab.com>
2021-10-21 13:33:42 -0700

    Grading PR

--- sql/setup.sql
+++ sql/setup.sql
@@ -24,8 +24,18 @@ CREATE TABLE menus(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id TEXT REFERENCES users(google_id),
     meal_name TEXT NOT NULL unique,
-    ingredients TEXT [] NOT NULL
+    ingredients TEXT [] NOT NULL -- Perhaps this should be a junction table instead?
 );
+
+-- Something like:
+-- CREATE TABLE menu_ingredients (
+--     inventory_id BIGINT,
+--     menu_id BIGINT,
+--     quantity DECIMAL,
+--     FOREIGN KEY (inventory_id) REFERENCES inventory(id),
+--     FOREIGN KEY (menu_id) REFERENCES menus(id),
+-- )
+-- And any other fields you need to track which ingredients go into each menu item
 
 CREATE TABLE inventory_menus(
     inventory_id BIGINT REFERENCES inventory(id),
@@ -35,5 +45,5 @@ CREATE TABLE inventory_menus(
 CREATE TABLE sales(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id TEXT REFERENCES users(google_id),
-    sales TEXT []
+    sales TEXT [] -- Instead of an array, how else might you handle sales data?
 );
